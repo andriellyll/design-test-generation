7abfd9331283e9548523b80b17bf3b0366ae5c10
Andrielly Lucena <51917545+andriellyll@users.noreply.github.com>
2025-08-29 09:00:53 -0300

    Update connection string with new password

--- scripts/constants.py
+++ scripts/constants.py
@@ -1,4 +1,4 @@
-connection_string = "postgresql://postgres:postgres@localhost:5432"
+connection_string = "postgresql://postgres:mysecretpassword@localhost:5432"
 db_name = "design_wizard_documentation"
 table_name="docs"
 contents_dir="./data/doc/org/designwizard/design"
